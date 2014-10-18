4c9d5d7618df6d36367aac2798a3bc21f887e882
Aquima <Quima@Aquima.local>
2014-10-18 01:26:38 -0500

    Init Dev

---
+++ TestSydney/TestSydney/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSydney
+//
+//  Created by Raul Quispe on 10/18/14.
+//  Copyright (c) 2014 kodebinario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestSydney/TestSydney/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestSydney
+//
+//  Created by Raul Quispe on 10/18/14.
+//  Copyright (c) 2014 kodebinario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
